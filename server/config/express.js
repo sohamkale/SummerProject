f2913a7ad8a1d4ec15d0e3993c4f2418b803ec61
sohamkale <sohamkale@ufl.edu>
2020-05-21 07:31:24 -0400

    deployment test

--- server/config/express.js
+++ server/config/express.js
@@ -32,17 +32,6 @@ module.exports.init = () => {
 
     // add a router
     app.use('/api/example', exampleRouter);
-
-    if (process.env.NODE_ENV === 'production') {
-        // Serve any static files
-        app.use(express.static(path.join(__dirname, '../../client/build')));
-
-        // Handle React routing, return all requests to React app
-        app.get('*', function(req, res) {
-            res.sendFile(path.join(__dirname, '../../client/build', 'index.html'));
-        });
-    }
-
     return app
 }
 
