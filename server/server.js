0874ea844c42df6f6e5c7f5ec070546ee1e07a00
sohamkale <sohamkale@ufl.edu>
2020-05-21 09:16:17 -0400

    This and its previous version works for adding users
    gives problems with GET requests on users

--- server/server.js
+++ server/server.js
@@ -18,6 +18,7 @@ const usersRouter = require('./routes/users');
 const postsRouter = require('./routes/posts');
 const commentRouter = require('./routes/comment');
 
+
 const app = express.init()
 app.use('/users', usersRouter);
 app.use('/posts', postsRouter);
