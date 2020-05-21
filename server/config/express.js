feeb3f845b54fb6a08cbdf3378909ca64254b2b7
sohamkale <sohamkale@ufl.edu>
2020-05-21 07:38:10 -0400

    Update express.js

--- server/config/express.js
+++ server/config/express.js
@@ -32,6 +32,13 @@ module.exports.init = () => {
 
     // add a router
     app.use('/api/example', exampleRouter);
+
+    if (process.env.NODE_ENV === "production"){
+        app.use(express.static("../../client/build"));
+        // app.get('*', function (req, res) {
+        //     res.sendFile(path.resolve(__dirname, '../client', '../client/build', 'index.html'));
+        // });
+    }
     return app
 }
 
