ea463163b06b0525411a2ae19860f2e5994c941d
mimmam1464 <m.immam@ufl.edu>
2020-05-24 14:52:41 -0400

    merger master

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,39 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 // import 'bootstrap/dist/css/bootstrap.css';
 import { Route, Switch, Redirect  } from 'react-router-dom';
-import NotFound from "./Components/Shared/NotFound";
+import NotFound from "./components/Shared/NotFound";
 // import NavBar from "./Components/Shared/NavBar";
 
-import Home from "./Views/Home/Home";
-import NavBar from "./Components/Header/NavBar";
-import LoginApp from "./Views/Login/LoginApp";
-import Signup from "./Views/Signup/Signup";
+import Home from "./views/Home/Home"
+import NavBar from "./components/Header/NavBar";
+import LoginApp from "./views/Login/LoginApp";
+import Signup from "./views/Signup/Signup";
+import usersFront from "./components/UsersFront";
+import fire from './config/Fire'
+
 const App = () => {
-  return (
+  useEffect(() => {
   
+  }, []);
+
+
+  return (
     //Imported public NavBar
     <div>
-      <head>
-        <link href="https://fonts.googleapis.com/css2?family=Sofia&family=Spartan:wght@400;700&display=swap" rel="stylesheet"></link>
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous"/>
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous"></link>
-      </head>
+    {
+       
+    }
       <NavBar />
       <Switch>
-      <Route exact path="/Home" component={Home} />
+      <Route exact path="/Home" render={(props) => <Home {...props} isLoggedIn={false} />}/>
       {/* <Route exact path="/contact" component={ContactMe} /> */}
       <Route exact path="/Login" component={LoginApp} />
       <Route exact path="/Signup" component={Signup} />
-
+      <Route exact path="/usersFront" component={usersFront} />
       {/* <Route exact path="/delete" component={DeleteImage}/> */}
       {/* remove this above line */}
       <Route exact path="/">
-        <Redirect to="/Home" />
+        <Redirect to="/Login" />
       </Route>
         <Route component={NotFound}/>
       </Switch>
